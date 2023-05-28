b5cfccd375d8ea6bc5ffa798e540d2c45203b101
Russell Blickhan <rwblickhan@gmail.com>
2023-05-28 11:47:07 -0700

    Initial Commit

---
+++ AskholeMobile/AskholeMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskholeMobileApp.swift
+//  AskholeMobile
+//
+//  Created by Russell Blickhan on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct AskholeMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
