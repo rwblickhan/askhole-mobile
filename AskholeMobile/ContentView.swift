6532c0f81e7641e079e7a28288286e2197ee7800
Russell Blickhan <rwblickhan@gmail.com>
2023-05-29 14:37:30 -0700

    Add top spacer as well

--- AskholeMobile/ContentView.swift
+++ AskholeMobile/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
+            Spacer()
             Text(items[currentQuestion].question)
                 .padding()
             Spacer()
