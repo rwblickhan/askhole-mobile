3f568f53cfeca01fc0ce4885d0d5c8afeba44eeb
Russell Blickhan <rwblickhan@gmail.com>
2023-05-28 21:32:49 -0700

    Stub out project

---
+++ AskholeMobile/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  AskholeMobile
+//
+//  Created by Russell Blickhan on 5/28/23.
+//
+
+import Foundation
+
+struct Question: Codable {
+    let question: String
+    let id: Int
+}
